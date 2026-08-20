59e0b64f37a7313d0c9f99dec848215cf7a29ff6
Tetiana Sobolieva <tetiana.sobolieva.s@gmail.com>
2026-08-20 19:46:11 +0300

    started frontend: config + index

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true },
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint', 'react-hooks', 'react-refresh', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  env: { browser: true, es2021: true, node: true },
+  rules: {
+    'prettier/prettier': 'warn',
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+  },
+  ignorePatterns: ['dist', 'node_modules'],
+};
